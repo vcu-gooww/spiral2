00a91bc9fdaa1e27221c02d618c5d04a6784dcc0
vcu-gooww <78395107+vcu-gooww@users.noreply.github.com>
2021-05-04 22:19:41 -0400

    Update homework.py

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,20 @@
 def spiralize(size, n=1):
-    """ Your code goes somewhere in here"""
-    return_value = n
+    
+    sum = []
+    for n in range(1, 501, 2):
+        
+    a = n**2 #a is top right diagonal
+    
+    b = a - (n-1) #b is top left diagnoal
+    
+    c = b - (n-1) #c is bottom left diagonal
+    
+    d = c - (n-1) #d is bottom right diagonal
+    
+        Diagonal_Sum = a + b + c +d
+        
+    sum.append(Diagonal_Sum)
+    
+    return_value = sum(sum) 
+    
     return return_value
